5db414d30c3ac6a097a3aa497e56476a72aa9784
maijun <389176868@qq.com>
2017-04-21 15:55:00 +0800

    写个异步

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var path = require('path');
-var fs = require('fs');
+let fs = require('fs');
+let path = require('path');
+let	readFilePromise = function (fileName) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(fileName, (err, data) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(data.toString())
+			}
+		})
+	})
+}
 
 // this is my first github Item
